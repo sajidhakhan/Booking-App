04b2752e3a28b32a6b732642c9a5fc30277e7d01
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-08-17 12:46:55 +0530

    backend for login and registration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,19 +2,23 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
+import authRoutes from './routes/auth';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
-
+// Log connection success or error
+mongoose.connection.on('connected', () => {
+    console.log('Successfully connected to MongoDB database');
+});
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response)=>{
-    res.json({ message: "hello from express endpoint!" });
-});
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(7000, ()=>{
     console.log("Server running on localhost:7000!");
--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { Children } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Footer from '../components/Footer';
