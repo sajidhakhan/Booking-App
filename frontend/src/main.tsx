941ee2452eef6fa5cacf4bf732d755123b53a2cd
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-08-17 18:29:47 +0530

    added toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,19 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 import './index.css';
+import { AppContextProvider } from './contexts/AppContent';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+    <AppContextProvider>
+    <BrowserRouter>
         <App />
       </BrowserRouter>
+    </AppContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
