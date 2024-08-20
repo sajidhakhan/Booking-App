edb3dc576aa4302da96d5dae90515236c303e2d3
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-08-20 11:43:31 +0530

    removed unnecessary imports

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
     
@@ -22,10 +23,11 @@ app.use(
     credentials: true,
 }));
 
-
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+
 
 app.listen(7000, () => {
     console.log("Server running on localhost:7000!");
