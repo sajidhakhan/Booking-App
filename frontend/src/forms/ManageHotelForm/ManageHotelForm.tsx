29d362f1000be6bfc0f35ce4577e17dd4093ac42
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-08-21 17:11:24 +0530

    add hotel feature and tests added

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -19,7 +19,12 @@ export type HotelFormData = {
     childCount: number;
 }
 
-const ManageHotelForm = () => {
+type Props = {
+    onSave: (hotelFormData: FormData) => void
+    isLoading: boolean
+}
+
+const ManageHotelForm = ({onSave, isLoading}: Props) => {
     const formMethods = useForm<HotelFormData>();
     const { handleSubmit } = formMethods;
 
@@ -42,6 +47,8 @@ const ManageHotelForm = () => {
         Array.from(formDataJson.imageFiles).forEach((imageFile)=>{
             formData.append(`imageFiles`, imageFile);
         });
+
+        onSave(formData);
     });
 
     return (
@@ -53,8 +60,11 @@ const ManageHotelForm = () => {
                 <GuestsSection />
                 <ImagesSection />
                 <span className="flex justify-end">
-                    <button type="submit" className="bg-blue-800 text-white p-2 font-bold hover:bg-blue-500 text-xl">
-                        Save
+                    <button 
+                    disabled={isLoading}
+                    type="submit"
+                    className="bg-blue-800 text-white p-2 font-bold hover:bg-blue-500 text-xl disabled:bg-gray-500">
+                        {isLoading? "Saving..." : "Save"}
                     </button>
                 </span>
             </form>
--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,8 +1,27 @@
+import { useMutation } from "react-query";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
-
+import { useAppContext } from "../contexts/AppContext";
+import * as apiCilent from '../api-client';
 const AddHotel = () => {
+
+  const  { showToast } = useAppContext();
+  const { mutate, isLoading} = useMutation(apiCilent.addMyHotel,{
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved!", type: "SUCCESS"});
+    },
+    onError: () =>{
+      showToast({ message: "Error Saving Hotel", type: "ERROR" });
+    }
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  }
+
+
+
   return (
-    <ManageHotelForm />
+    <ManageHotelForm  onSave={handleSave}  isLoading={isLoading} />
   )
 }
 
