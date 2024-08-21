81c2c332c94279ae36e1582d0baabee1d41589eb
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-08-21 10:47:47 +0530

    hotel route added

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -1,7 +1,9 @@
 import  express, { Request, Response}  from "express";
 import  multer from 'multer';
 import cloudinary from 'cloudinary';
-import { HotelType } from "../models/hotel";
+import Hotel, { HotelType } from "../models/hotel";
+import verifyToken from "../middleware/auth";
+import { body } from "express-validator";
 
 const router = express.Router();
 
@@ -16,14 +18,21 @@ const upload = multer({
 // api/my-hotels
 router.post(
     "/", 
+    verifyToken, [
+        body("name").notEmpty().withMessage('Name is required'),
+        body("city").notEmpty().withMessage('City is required'),
+        body("country").notEmpty().withMessage('Country is required'),
+        body("description").notEmpty().withMessage('Description is required'),
+        body("type").notEmpty().withMessage('Hotel type is required'),
+        body("pricePerNight").notEmpty().isNumeric().withMessage('Price Per Night is required nad must be a number'),
+        body("facilities").notEmpty().isArray().withMessage('Facilities is required'),
+    ],
     upload.array("imageFiles",6), 
     async (req: Request, res: Response) =>{
         try {
             const imageFiles = req.files as Express.Multer.File[];
             const newHotel: HotelType = req.body;
             
-
-
             const uploadPromises = imageFiles.map(async(image) =>{
                 const b64 = Buffer.from(image.buffer).toString("base64");
                 let dataURI= "data:" + image.mimetype + ";base64," + b64;
@@ -36,8 +45,14 @@ router.post(
             newHotel.lastUpdated = new Date();
             newHotel.userId = req.userId;
 
+            const hotel = new Hotel(newHotel);
+            await hotel.save();
+
+            res.status(201).send(hotel);
         } catch (error) {
             console.log("Error creating hotel:", error);
             res.status(500).json({ message: "Something went wrong" });
         }
-})
+});
+
+export default router;
