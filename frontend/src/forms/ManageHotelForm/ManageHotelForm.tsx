21ae136a793dc86116694ca044dafced3b25d0cb
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-08-21 15:39:05 +0530

    form data added

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -23,9 +23,26 @@ const ManageHotelForm = () => {
     const formMethods = useForm<HotelFormData>();
     const { handleSubmit } = formMethods;
 
-    const onSubmit = (formData: HotelFormData) => {
-        console.log(formData);
-    };
+    const onSubmit = ((formDataJson: HotelFormData) => {
+        const formData = new FormData();
+        formData.append("name",formDataJson.name);
+        formData.append("city",formDataJson.city);
+        formData.append("country",formDataJson.country);
+        formData.append("description",formDataJson.description);
+        formData.append("type",formDataJson.type);
+        formData.append("pricePerNight",formDataJson.pricePerNight.toString());
+        formData.append("starRating",formDataJson.starRating.toString());
+        formData.append("adultCount",formDataJson.adultCount.toString());
+        formData.append("childCount",formDataJson.childCount.toString());
+        
+        formDataJson.facilities.forEach((facility, index) =>{
+            formData.append(`facilities[${index}]`,facility)
+        });
+
+        Array.from(formDataJson.imageFiles).forEach((imageFile)=>{
+            formData.append(`imageFiles`, imageFile);
+        });
+    });
 
     return (
         <FormProvider {...formMethods}>
