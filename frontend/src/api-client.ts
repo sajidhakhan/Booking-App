1ab100dead353f7914698fdc77c5091bba20edf4
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-09-05 18:36:32 +0530

    removed unnecessary import

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,7 +1,6 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
 import  { HotelSearchResponse, HotelType } from '../../backend/src/shared/types';
-import { Await } from "react-router-dom";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
